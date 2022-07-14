ccfab9acea9b0e3596ceb0f9ab451eb6e8e9e1ea
efurst <efurst1@depaul.edu>
2022-07-14 15:37:32 -0500

    updated OnDemandWaterbirdsDataset

--- datasets.py
+++ datasets.py
@@ -59,35 +59,36 @@ class SubclassedNoduleDataset(Dataset):
 
 
 class OnDemandWaterbirdsDataset(Dataset):
-    def __init__(self, metadata, device):
+    def __init__(self, metadata, root_dir, transform, device):
         '''
         INPUTS:
-        metadata: metadata csv storing the image paths, labels, and subclasses
+        metadata: metadata dataframe storing the image paths, labels, and subclasses
+        root_dir: the directory where the image files are stored
+        transform: the transform to apply to the image when it is loaded
         device to move tensors to as they are loaded
 
         '''
 
-        self.metadata_df = pd.read_csv(metadata)
+        self.metadata = metadata
+        self.root_dir = root_dir
+        self.transform = transform
         self.device = device
 
-        # Image transformation for loading images
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((200, 200), antialias=True)])
-
     def __len__(self):
-        return len(self.metadata_df)
+        return len(self.metadata)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         # column 1: image path
-        img_path = self.metadata_df.iloc[idx, 1]
-        img_tensor = self.transform(Image.open(img_path)).to(self.device)
+        img_path = self.metadata.iloc[idx, 1]
+        img_tensor = self.transform(Image.open(self.root_dir + img_path)).squeeze().to(self.device)
 
         # column 2: image label
-        label = torch.IntTensor(self.metadata_df.iloc[idx, 2]).to(self.device)
+        label = torch.LongTensor([self.metadata.iloc[idx, 2]]).squeeze().to(self.device)
 
         # column 4 contains the confounding label, which is combined with column 2 to get the subclass
-        subclass = torch.IntTensor(2 * self.metadata_df.iloc[idx, 2] + self.metadata_df.iloc[idx, 4]).to(self.device)
+        subclass = torch.LongTensor([2 * self.metadata.iloc[idx, 2] + self.metadata.iloc[idx, 4]]).squeeze().to(self.device)
 
         return img_tensor, label, subclass
