95690510446e4ed5b1d4c12333e23e49a3ca5069
Thomas Zeng <tomzeng200@gmail.com>
2022-07-18 10:32:40 -0500

    fixes train split

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -28,8 +28,8 @@ for malig in range(4):
     maligs.extend((malig,)*length)
 
 lidc['spic_b'] = np.where(lidc['spiculation'] > 1, 1, 0)
-spics = [m*2+s for m,s in zip(maligs, lidc['spic_b'])]
-maligs_b = list(map( lambda x:int(x>1), maligs))
+maligs_b = list(map( lambda x:int(x>3), maligs))
+spics = [m*2+s for m,s in zip(maligs_b, lidc['spic_b'])]
 df_split = pd.DataFrame(zip(noduleID, spics, maligs, maligs_b, split), columns=['noduleID', 'spic_groups', 'malignancy', 'malignancy_b', 'split'])
 
 # df_split = pd.DataFrame(zip(noduleID, maligs, maligs, split), columns=['noduleID', 'subgroup', 'malignancy', 'split'])
