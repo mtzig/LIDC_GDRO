296bc78633981e2ec7f47c56b693f31a0ee27938
efurst <efurst1@depaul.edu>
2022-07-19 16:16:05 -0500

    removed hparams from ermloss

--- scripts/run.py
+++ scripts/run.py
@@ -1,18 +1,35 @@
 import torch
 import loss
+import models
+import argparse
 
 # hyperparameters
+
 lr = 0.0005
 wd = 0.005
 eta = 0.01
 gamma = 1.0
 
+model_class = models.NeuralNetwork
 optimizer_class = torch.optim.Adam
-loss_fn_class = loss.ERMLoss
+erm_class = loss.ERMLoss
+gdro_class = loss.GDROLoss
+dynamic_class = loss.DynamicLoss
+upweight_class = loss.UpweightLoss
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+trials = 100
+epochs = 100
 batch_size = 128
 split_path = '../data/train_test_splits/LIDC_data_split.csv'
 subclass_path = '../data/subclass_labels/LIDC_data_split_with_cluster.csv'
 subclass_column = 'spic_groups'
 feature_path = '../data/LIDC_designed_features.csv'
 
+def main():
+    print("Hello world")
+
+
+if __name__ == '__main__':
+    main()
