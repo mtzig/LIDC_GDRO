7f93818f9b9676340d4e8dceb50ebeaea98c95a4
Thomas Zeng <tomzeng200@gmail.com>
2022-07-21 14:58:50 -0500

    fix naming error

--- scripts/clusterer.py
+++ scripts/clusterer.py
@@ -26,7 +26,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 now = datetime.now()
 results_dir = f'cluster_{now.strftime("%Y%m%d_%H%M%S")}'
-os.mkdir(f'./data/results_dir')
+os.mkdir(f'./data/{results_dir}')
 images_df = images_to_df()
 train_data, cv_data, test_data = get_features(images=True, features=images_df, device=DEVICE, subclass='malignancy')
 
