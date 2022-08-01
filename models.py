0028a7e929b06df13a0107c86e510dad544b976e
Thomas Zeng <tomzeng200@gmail.com>
2022-08-01 14:11:53 -0500

    readds weight declaration

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class TransferModel18(nn.Module):
         super(TransferModel18, self).__init__()
 
         if pretrained:
-            self.model = torchvision.models.resnet18('DEFAULT').to(device)
+            self.model = torchvision.models.resnet18(weights='DEFAULT').to(device)
         else:
             self.model = torchvision.models.resnet18().to(device)
 
