278cc6ba71c9eda558865d679286e1549a27a930
Wang <YWANG192@depaul.edu>
2022-12-15 16:21:00 -0600

    'Cleaned_version_Ian_experiment'

---
+++ run_validation.py
@@ -0,0 +1,217 @@
+import torch
+from loss import ERMLoss, GDROLoss
+import models
+from utils import data_utils, image_data_utils
+from train_eval import run_trials_images,run_trials_designedFeatures,run_trials_CNN
+import pandas as pd
+from datetime import datetime
+import os
+import argparse
+from dataloaders import InfiniteDataLoader
+from datasets import SubclassedDataset
+import glob
+import tensorflow as tf
+import torch.optim.lr_scheduler as schedulers
+import torch.optim as optimizers
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('stratification', help='The stratification type to use, available options are "spic_groups", "malignancy", and "cluster"')
+parser.add_argument('--test_name', '-n', default='test', help='The name of the test to run (defaults to "test"), the output files will be saved in the directory ./test_results/[name]/')
+# parser.add_argument('--cnn', action='store_true', help='Use the CNN (deep) feature data representation')
+parser.add_argument('--e2e', action='store_true', help='Use the image data representation (train CNN "end to end")')
+parser.add_argument('--designed', '-d', action='store_true', help='Use the designed feature data representation (optional, if no data representation is specified designed is used by default)')
+parser.add_argument('--verbose', '-v', action='store_true', help='Print the progress')
+parser.add_argument('--colab', '-c', action='store_true', help='Add this argument to properly download the results if the script is being run through Google Colab')
+parser.add_argument('--trials', '-t', default=30, help='The number of trials to run, defaults to 30')
+
+args = parser.parse_args()
+
+
+# hyperparameters
+
+lr_erm = 0.0005
+lr_dro = 0.0005
+wd_erm = 0.005
+wd_dro = 0.005
+eta = 0.01
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+if args.e2e:
+    model_class = models.TransferModel18
+    model_args = [True, False, device]
+else:
+    model_class = models.NeuralNetwork
+    model_args = [64, 36, 2]
+
+optimizer_class = torch.optim.Adam
+
+trials = int(args.trials)
+
+epochs = 45
+batch_size = 128
+
+subclass_column = args.stratification
+split_path = "train_test_splits/Nodule_Level_30Splits/nodule_split_1.csv"
+subclass_path = 'subclass_labels/subclasses.csv'
+feature_path = 'LIDC_20130817_AllFeatures2D_MaxSlicePerNodule_inLineRatings.csv'
+results_root_dir = 'test_results/'
+test_name = args.test_name
+verbose = args.verbose
+
+num_subclasses = 4
+subtypes = ["Overall"]
+if subclass_column == 'cluster':
+    subtypes.extend(["Predominantly Benign", "Somewhat Benign", "Somewhat Malignant", "Predominantly Malignant"])
+elif subclass_column == 'spic_groups':
+    subtypes.extend(["Unspiculated benign", "Spiculated benign", "Spiculated malignant", "Unspiculated malignant"])
+elif subclass_column == 'malignancy':
+    subtypes.extend(["Highly Unlikely", "Moderately Unlikely", "Moderately Suspicious", "Highly Suspicious"])
+else:
+    subtypes.extend(list(range(num_subclasses)))
+
+
+if args.e2e:
+
+    erm_class = ERMLoss
+    erm_args = [None, torch.nn.CrossEntropyLoss()]
+    gdro_class = GDROLoss
+    gdro_args = [None, torch.nn.CrossEntropyLoss(), eta, num_subclasses]
+    optimizer_args_erm = {'lr': lr_erm, 'weight_decay': wd_erm}
+    optimizer_args_dro = {'lr': lr_dro, 'weight_decay': wd_dro}
+    results = {"Accuracies": {}, "Accuracies_Train": {},"Accuracies_Validation": {}}
+    BEST_EPOCH_RESULTS = {"Best_Test_Accuracy": {}}
+
+    for loss_class, loss_args in zip([erm_class, gdro_class], [erm_args, gdro_args]):
+
+        fn_name = loss_class.__name__
+
+        if verbose:
+            print(f"Running trials: {fn_name}")
+
+        accuracies, accuracies_train,accuracies_val,best_test_accuracies = run_trials_images(
+        
+            epochs=epochs,
+            csv_files = glob.glob(os.path.join('./data/train_test_splits/Nodule_Level_30Splits/', "*.csv")),
+            fn_name = fn_name,
+            batch_size = batch_size,
+            subclass_column = subclass_column,
+            model_class=model_class,
+            model_args=model_args,
+            loss_class=loss_class,
+            loss_args=loss_args,
+            optimizer_class=optimizer_class,
+            optimizer_args_erm=optimizer_args_erm,
+            optimizer_args_dro=optimizer_args_dro,
+            device=device,
+            verbose=verbose,
+            record=True,
+        
+        )
+        results["Accuracies"][fn_name] = accuracies
+        results["Accuracies_Train"][fn_name] = accuracies_train 
+        results["Accuracies_Validation"][fn_name] = accuracies_val
+        BEST_EPOCH_RESULTS["Best_Test_Accuracy"][fn_name] = best_test_accuracies
+
+if args.designed:
+
+    df = data_utils.preprocess_data(
+        *data_utils.load_lidc(
+            data_root='data/',
+            feature_path=feature_path,
+            subclass_path=subclass_path
+        ),
+        subclass_column=subclass_column
+        )
+    
+    erm_class = ERMLoss
+    erm_args = [None, torch.nn.CrossEntropyLoss()]
+    gdro_class = GDROLoss
+    gdro_args = [None, torch.nn.CrossEntropyLoss(), eta, num_subclasses]
+
+    optimizer_args_erm = {'lr': lr_erm, 'weight_decay': wd_erm}
+    optimizer_args_dro = {'lr': lr_dro, 'weight_decay': wd_dro}
+
+    results = {"Accuracies": {}, "Accuracies_Train": {},"Accuracies_Validation": {}}
+    BEST_EPOCH_RESULTS = {"Best_Test_Accuracy": {}}
+
+    for loss_class, loss_args in zip([erm_class, gdro_class], [erm_args, gdro_args]):
+        fn_name = loss_class.__name__
+        if verbose:
+            print(f"Running trials: {fn_name}")
+        
+        accuracies, accuracies_train,accuracies_val,best_test_accuracies = run_trials_designedFeatures(
+            epochs=epochs,
+            df = df,
+            csv_files = glob.glob(os.path.join('./data/train_test_splits/Nodule_Level_30Splits/', "*.csv")),
+            fn_name = fn_name,
+            batch_size = batch_size,
+            model_class=model_class,
+            model_args=model_args,
+            loss_class=loss_class,
+            loss_args=loss_args,
+            optimizer_class=optimizer_class,
+            optimizer_args_erm=optimizer_args_erm,
+            optimizer_args_dro=optimizer_args_dro,
+            device=device,
+            verbose=verbose,
+            record=True,
+            num_subclasses=num_subclasses
+        )
+        results["Accuracies"][fn_name] = accuracies
+        results["Accuracies_Train"][fn_name] = accuracies_train 
+        results["Accuracies_Validation"][fn_name] = accuracies_val
+        BEST_EPOCH_RESULTS["Best_Test_Accuracy"][fn_name] = best_test_accuracies
+
+
+    
+accuracies_df_test = pd.DataFrame(
+results["Accuracies"],
+index=pd.MultiIndex.from_product(
+    [range(30), range(epochs), subtypes],
+    names=["trial", "epoch", "subtype"]
+    )
+    )
+
+accuracies_df_train = pd.DataFrame(
+results["Accuracies_Train"],
+index=pd.MultiIndex.from_product(
+    [range(30), range(epochs), subtypes],
+    names=["trial", "epoch", "subtype"]
+    )
+    )
+
+
+accuracies_df_Validation = pd.DataFrame(
+results["Accuracies_Validation"],
+index=pd.MultiIndex.from_product(
+    [range(30), range(epochs), subtypes],
+    names=["trial", "epoch", "subtype"]
+    )
+    )
+
+accuracies_BEST_df = pd.DataFrame(
+BEST_EPOCH_RESULTS["Best_Test_Accuracy"],
+index=pd.MultiIndex.from_product(
+    [range(30),subtypes],
+    names=["trial", "subtype"]
+    )   
+    )
+
+now = datetime.now()
+results_dir = results_root_dir + f'{test_name}_{now.strftime("%Y%m%d_%H%M%S")}/'
+if not os.path.isdir(results_dir):
+    os.mkdir(results_dir)
+
+accuracies_df_test.to_csv(results_dir + f'test_accuraciesPerEpoch.csv')
+accuracies_df_train.to_csv(results_dir + f'train_accuraciesPerEpoch.csv')
+accuracies_df_Validation.to_csv(results_dir + f'validation_accuraciesPerEpoch.csv')
+accuracies_BEST_df.to_csv(results_dir + f'BEST_accuracies.csv')
+
+
+if args.colab:
+    from google.colab import files
+    files.download(results_dir + f'accuracies.csv')
+    files.download(results_dir + f'q.csv')
+    files.download(results_dir + f'roc.csv')
--- train_eval.py
+++ train_eval.py
@@ -3,6 +3,11 @@ import numpy as np
 import torch
 from loss import GDROLoss
 
+from utils import data_utils, image_data_utils
+from dataloaders import InfiniteDataLoader
+from datasets import SubclassedDataset
+import torch.optim.lr_scheduler as schedulers
+
 
 def train(dataloader, model, loss_fn, optimizer, verbose=False):
     """
@@ -73,15 +78,20 @@ def evaluate(dataloader, model, num_subclasses, verbose=False):
 
 
 def train_epochs(epochs,
+                 erm_flag,
                  train_dataloader,
+                 val_dataloader,
                  test_dataloader,
+                 eval_train_dataloader,
                  model,
                  loss_fn,
                  optimizer,
+                 device,
                  scheduler=None,
                  verbose=False,
                  record=False,
-                 num_subclasses=1):
+                 num_subclasses=4
+                 ):
     """
     Trains the model for a number of epochs and evaluates the model at each epoch
     :param epochs: The number of epochs to train
@@ -98,34 +108,332 @@ def train_epochs(epochs,
     """
     if record:
         accuracies = []
-        q_data = None
-        if isinstance(loss_fn, GDROLoss):
-            q_data = []
+        accuracies_train = []
+        accuracies_validation = []
+        accuracies_BEST = []
+        max_val_accuacy = 0
+        max_worst_accuracy = 0
+        best_epoch = 0
+        # q_data = None
+        # if isinstance(loss_fn, GDROLoss):
+        #     q_data = []
 
     for epoch in range(epochs):
         if verbose:
             print(f'Epoch {epoch + 1} / {epochs}')
 
         train(train_dataloader, model, loss_fn, optimizer)
-        if scheduler:
-            scheduler.step(evaluate(test_dataloader, model, num_subclasses=num_subclasses)[0])
+
+        temp_val_accuracies = evaluate(val_dataloader, model, num_subclasses=num_subclasses)
+        temp_val_accuracy = temp_val_accuracies[0]
+        temp_worst_accuracy = min(temp_val_accuracies[1:])
+        
+        print('val_overall: ', temp_val_accuracy)
+        print('val_worst: ', temp_worst_accuracy)
+        
+
+        if erm_flag:           
+            if temp_val_accuracy >= max_val_accuacy:
+                max_val_accuacy = temp_val_accuracy
+                best_epoch = epoch
+                print('I am saving the best ERM model at Epoch: ',best_epoch)
+                torch.save(model.state_dict(), 'D:\LIDC_GDRO_UseValidation\Best_model.pth')
+
+        else:
+            if temp_worst_accuracy >= max_worst_accuracy:
+                max_worst_accuracy = temp_worst_accuracy
+                best_epoch = epoch
+                print('I am saving the best dro model at Epoch: ', best_epoch)
+                torch.save(model.state_dict(), 'D:\LIDC_GDRO_UseValidation\Best_model.pth')
+
+            #  if temp_val_accuracy >= max_val_accuacy:
+            #     max_val_accuacy = temp_val_accuracy
+            #     best_epoch = epoch
+            #     print('I am saving the best dro model at Epoch: ',best_epoch)
+            #     torch.save(model.state_dict(), 'D:\LIDC_GDRO_Debug\Best_model.pth')
+
+        if scheduler and erm_flag:
+            scheduler.step(evaluate(val_dataloader, model, num_subclasses=num_subclasses)[0])
+            print('ERM Learning learning rate is: ', optimizer.param_groups[0]['lr'])
+        
+        if scheduler and not erm_flag:
+            scheduler.step(min(evaluate(val_dataloader, model, num_subclasses=num_subclasses)[1:]))
+            print('DRO Learning learning rate is: ', optimizer.param_groups[0]['lr'])
 
         if record:
             epoch_accuracies = evaluate(test_dataloader, model, num_subclasses=num_subclasses)
+            epoch_accuracies_train = evaluate(eval_train_dataloader, model, num_subclasses=num_subclasses)
+            epoch_accuracies_val = evaluate(val_dataloader, model, num_subclasses=num_subclasses)
             accuracies.extend(epoch_accuracies)
-            if isinstance(loss_fn, GDROLoss):
-                q_data.extend(loss_fn.q.tolist())
+            accuracies_train.extend(epoch_accuracies_train)
+            accuracies_validation.extend(epoch_accuracies_val)
+    
+    # Loading
+    
+
+    model.load_state_dict(torch.load('D:\LIDC_GDRO_UseValidation\Best_model.pth'))
+    model = model.to(device)
+    best_test_accuracies = evaluate(test_dataloader, model, num_subclasses=num_subclasses)
+    accuracies_BEST.extend(best_test_accuracies)
+
+           
 
     if record:
-        return accuracies, q_data
+        return accuracies, accuracies_train, accuracies_validation,accuracies_BEST
     else:
         return None
 
 
-def run_trials(num_trials,
+
+
+def generate_dataloaders_designedFeatures (df, individual_split_path, fn_name,batch_size):
+    
+    train, val, test = data_utils.train_val_test_datasets(df, split_path=individual_split_path)
+    
+    val_dataloader = InfiniteDataLoader(SubclassedDataset(*val), len(val))
+    test_dataloader = InfiniteDataLoader(SubclassedDataset(*test), len(test))
+    eval_train_dataloader = InfiniteDataLoader(SubclassedDataset(*train), len(train))
+    num_subclasses = len(test_dataloader.dataset.subclasses.unique())
+    
+    tr = SubclassedDataset(*train)
+    
+    if fn_name == 'ERMLoss':
+        train_dataloader = InfiniteDataLoader(tr, batch_size)
+    else:
+        train_dataloader = InfiniteDataLoader(tr, batch_size, weights=image_data_utils.get_sampler_weights(tr.subclasses))
+    
+    return train_dataloader, val_dataloader, test_dataloader, num_subclasses, eval_train_dataloader
+
+
+
+
+
+
+def run_trials_designedFeatures(
                epochs,
-               train_dataloader,
-               test_dataloader,
+               df,
+               csv_files,
+               fn_name,
+               batch_size,
+            #    train_dataloader,
+            #    test_dataloader,
+               model_class,
+               model_args,
+               loss_class,
+               loss_args,
+               optimizer_class,
+               optimizer_args_erm,
+               optimizer_args_dro,
+               device='cpu',
+               num_subclasses=1,
+               scheduler=None,
+               verbose=False,
+               record=False
+               ):
+    if record:
+        accuracies = []
+        accuracies_train = []
+        accuracies_val = []
+        accuracies_test_BEST = []
+        # roc_data = [None, None]
+        # q_data = None
+        # g_data = None
+        # if loss_class is GDROLoss:
+        #     q_data = []
+
+    num_trials = len(csv_files)
+    for n in range(num_trials):
+        if verbose:
+            print(f"Trial {n + 1}/{num_trials}")
+
+        individual_split_path = csv_files[n]
+        train_dataloader, val_dataloader, test_dataloader, num_subclasses,eval_train_dataloader = generate_dataloaders_designedFeatures(df, individual_split_path, fn_name,batch_size)        
+        
+        model = model_class(*model_args).to(device)
+        loss_args[0] = model
+        loss_fn = loss_class(*loss_args)
+        
+
+        if fn_name == 'ERMLoss':
+            erm_flag = True
+            optimizer = optimizer_class(model.parameters(), **optimizer_args_erm)
+        else:
+            erm_flag = False
+            optimizer = optimizer_class(model.parameters(), **optimizer_args_dro)
+        
+        scheduler = init_scheduler({'class_args': {'patience':10,'factor': 0.1,'mode':'min'},'class_name': 'ReduceLROnPlateau'},optimizer)
+
+
+
+        trial_results = train_epochs(epochs,
+                                     erm_flag,
+                                     train_dataloader,
+                                     val_dataloader,
+                                     test_dataloader,
+                                     eval_train_dataloader,
+                                     model,
+                                     loss_fn,
+                                     optimizer,
+                                     device,
+                                     scheduler,
+                                     verbose,
+                                     record,
+                                     num_subclasses
+                                    )
+
+        if record:
+            trial_accuracies, trial_accuracies_train,trial_accuracies_val,best_test_accuracies = trial_results
+            accuracies.extend(trial_accuracies)
+            accuracies_train.extend(trial_accuracies_train)
+            accuracies_val.extend(trial_accuracies_val)
+            accuracies_test_BEST.extend(best_test_accuracies)
+
+    if record:
+        return accuracies, accuracies_train, accuracies_val,accuracies_test_BEST
+    else:
+        return None
+
+def init_scheduler(scheduler_config, optimizer):  
+    
+        scheduler_class = getattr(schedulers, scheduler_config['class_name'])
+        return scheduler_class(optimizer, **scheduler_config['class_args'])
+    
+
+
+
+def run_trials_images(
+               epochs,
+               csv_files,
+               fn_name,
+               batch_size,
+               subclass_column,
+            #    train_dataloader,
+            #    test_dataloader,
+               model_class,
+               model_args,
+               loss_class,
+               loss_args,
+               optimizer_class,
+               optimizer_args_erm,
+               optimizer_args_dro,
+               device='cpu',
+             
+            #    scheduler=True,
+               verbose=False,
+               record=False
+               ):
+    if record:
+        accuracies = []
+        accuracies_train = []
+        accuracies_val = []
+        accuracies_test_BEST = []
+        # roc_data = [None, None]
+        # q_data = None
+        # g_data = None
+        # if loss_class is GDROLoss:
+        #     q_data = []
+
+    
+    
+    num_trials = len(csv_files)
+
+    for n in range(num_trials):
+        if verbose:
+            print(f"Trial {n + 1}/{num_trials}")
+
+        individual_split_path = csv_files[n]
+        train, val, test = image_data_utils.get_features(split_file = individual_split_path,device=device, images=True, subclass=subclass_column)
+
+        val_dataloader = InfiniteDataLoader(SubclassedDataset(*val), len(val))
+        test_dataloader = InfiniteDataLoader(SubclassedDataset(*test), len(test))
+        eval_train_dataloader = InfiniteDataLoader(SubclassedDataset(*train), len(train))
+
+        num_subclasses = len(test_dataloader.dataset.subclasses.unique())
+
+        tr = SubclassedDataset(*train)
+
+        if fn_name == 'ERMLoss':
+            train_dataloader = InfiniteDataLoader(tr, batch_size)
+
+        else:
+            train_dataloader = InfiniteDataLoader(tr, batch_size, weights=image_data_utils.get_sampler_weights(tr.subclasses))
+
+
+        
+        model = model_class(*model_args).to(device)
+        loss_args[0] = model
+        loss_fn = loss_class(*loss_args)
+
+        if fn_name == 'ERMLoss':
+            optimizer = optimizer_class(model.parameters(), **optimizer_args_erm)
+        elif fn_name == 'GDROLoss':
+            optimizer = optimizer_class(model.parameters(), **optimizer_args_dro)
+        else:
+            print('ERROR!! IN run_trials_images')
+
+        scheduler = init_scheduler({'class_args': {'patience':10,'factor': 0.1,'mode':'min'},'class_name': 'ReduceLROnPlateau'},optimizer)
+
+
+        if fn_name == 'ERMLoss':
+            erm_flag = True
+        else:
+            erm_flag = False
+
+        trial_results = train_epochs(epochs,
+                                     erm_flag,
+                                     train_dataloader,
+                                     val_dataloader,
+                                     test_dataloader,
+                                     eval_train_dataloader,
+                                     model,
+                                     loss_fn,
+                                     optimizer,
+                                     device,
+                                     scheduler,
+                                     verbose,
+                                     record,
+                                     num_subclasses
+                                    )
+
+        if record:
+            trial_accuracies, trial_accuracies_train,trial_accuracies_val,best_test_accuracies = trial_results
+            accuracies.extend(trial_accuracies)
+            accuracies_train.extend(trial_accuracies_train)
+            accuracies_val.extend(trial_accuracies_val)
+            accuracies_test_BEST.extend(best_test_accuracies)
+
+    #         if isinstance(loss_fn, GDROLoss):
+    #             q_data.extend(trial_q_data)
+
+
+    #         with torch.no_grad():
+    #             preds = model(test_dataloader.dataset.features)
+    #             probabilities = torch.nn.functional.softmax(preds, dim=1)[:, 1]
+    #             if roc_data[0] is None:
+    #                 roc_data[0] = probabilities
+    #             else:
+    #                 roc_data[0] += probabilities
+    # if record:
+    #     roc_data[0] /= num_trials
+    #     labels = test_dataloader.dataset.labels
+    #     roc_data[1] = labels
+
+    if record:
+        return accuracies, accuracies_train, accuracies_val,accuracies_test_BEST
+    else:
+        return None
+
+
+
+
+def run_trials_CNN(
+               epochs,
+               csv_files,
+               fn_name,
+               batch_size,
+               subclass_column,
+            #    train_dataloader,
+            #    test_dataloader,
                model_class,
                model_args,
                loss_class,
@@ -133,7 +441,7 @@ def run_trials(num_trials,
                optimizer_class,
                optimizer_args,
                device='cpu',
-               num_subclasses=1,
+             
                scheduler=None,
                verbose=False,
                record=False
@@ -146,10 +454,31 @@ def run_trials(num_trials,
         if loss_class is GDROLoss:
             q_data = []
 
+    
+    
+    num_trials = len(csv_files)
+
     for n in range(num_trials):
         if verbose:
             print(f"Trial {n + 1}/{num_trials}")
 
+        individual_split_path = csv_files[n]
+        train, val, test = image_data_utils.get_features(split_file = individual_split_path,device=device,subclass=subclass_column)
+
+        val_dataloader = InfiniteDataLoader(SubclassedDataset(*val), len(val))
+        test_dataloader = InfiniteDataLoader(SubclassedDataset(*test), len(test))
+
+        num_subclasses = len(test_dataloader.dataset.subclasses.unique())
+
+        tr = SubclassedDataset(*train)
+
+        if fn_name == 'ERMLoss':
+            train_dataloader = InfiniteDataLoader(tr, batch_size)
+        else:
+            train_dataloader = InfiniteDataLoader(tr, batch_size, weights=image_data_utils.get_sampler_weights(tr.subclasses))
+
+
+        
         model = model_class(*model_args).to(device)
         loss_args[0] = model
         loss_fn = loss_class(*loss_args)
@@ -157,7 +486,8 @@ def run_trials(num_trials,
 
         trial_results = train_epochs(epochs,
                                      train_dataloader,
-                                     test_dataloader,
+                                    #  test_dataloader,
+                                     val_dataloader,
                                      model,
                                      loss_fn,
                                      optimizer,
